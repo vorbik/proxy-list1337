9af39a1ccb98429eae6daa59e0e00a3a94e9f6d8
VORBIK <83674294+VORBIK@users.noreply.github.com>
2026-05-02 17:31:49 +0300

    Create filter.py

---
+++ filter.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://raw.githubusercontent.com/zieng2/wl/main/vless_lite.txt"
+response = requests.get(url)
+
+if response.status_code == 200:
+    lines = response.text.splitlines()
+    filtered_lines = [line for line in lines if "finland" in line.lower() or "poland" in line.lower()]
+    
+    with open("filtered_vless.txt", "w") as f:
+        f.write("\n".join(filtered_lines))
